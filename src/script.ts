420668b9bd9891939f6c9816991960bedcda7cc3
Philip Saa <cowglow@gmail.com>
2025-10-03 01:14:14 +0200

    WIP: artist profiles - date generator

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,32 @@
-console.log("monstahz!!!")
+// import {mixLinks, nextRadioEvent} from "./artist-data/elektrikearliner.ts";
+
+const attachmentElement = document.getElementById("attachments")
+const mixListElement = document.getElementById("mix-list")
+const targetElement = document.getElementById("artist-key")
+
+const artistDataMap: Record<string, string> = {
+    elektrikearliner: "Elektrik Earliner",
+    tforce: "T-Force",
+}
+
+const params = new URLSearchParams(window.location.search);
+const artist = params.get("artist") ?? "Unknown Artist";
+
+if (attachmentElement && targetElement && mixListElement) {
+
+    targetElement.innerHTML = artistDataMap[artist]
+
+    // const pElement = document.createElement("p");
+    // pElement.innerHTML = `Next Radio Show: ${nextRadioEvent}`
+    // targetElement.appendChild(pElement);
+    //
+    // mixLinks.forEach(linkObj => {
+    //     const liElement = document.createElement("li");
+    //     liElement.innerHTML = `<a href="${linkObj.link}" target="_blank" rel="noopener">${linkObj.label}</a>`;
+    //     // const aElement = document.createElement("a");
+    //     // liElement.appendChild(aElement);
+    //     mixListElement.appendChild(liElement);
+    // });
+
+    console.log("monstahz!!!")
+}
---
+++ src/utils/get-next-radio-show-date.ts
@@ -0,0 +1,22 @@
+export function getNextRadioShowDate(dateNow: number): Date {
+    const currentDate = new Date(dateNow);
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth();
+
+    const firstDay = new Date(year, month, 1);
+    const weekday = firstDay.getDay(); // 0=Sun ... 6=Sat
+
+    const daysToFirstSaturday = (6 - weekday + 7) % 7;
+    const firstSaturdayDate = 1 + daysToFirstSaturday;
+
+    const fourthSaturday = new Date(year, month, firstSaturdayDate + 21);
+    fourthSaturday.setHours(22, 0, 0, 0)
+
+    // If it's already passed, get next month's 4th Saturday
+    if (fourthSaturday.getTime() < currentDate.getTime()) {
+        const nextDate = new Date(year, month + 1, 1)
+        return getNextRadioShowDate(nextDate.getTime());
+    }
+
+    return fourthSaturday;
+}
