360173c8e6c87844680d2c0423f81791ab461e98
Philip Saa <cowglow@gmail.com>
2026-03-14 12:21:13 +0100

    Using templates to render

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,13 @@
+import { renderTemplate } from "./utils/render-template.ts";
+
 console.log("monstahz!!!");
 
 // console.log(window.location.search)
 // const urlParms = new URLSearchParams(window.location.search)
 // console.log(urlParms.get("artist"))
+
+const headerEl = document.querySelector("header");
+const footerEl = document.querySelector("footer");
+
+if (headerEl) await renderTemplate("templates/header.html", headerEl);
+if (footerEl) await renderTemplate("templates/footer.html", footerEl);
