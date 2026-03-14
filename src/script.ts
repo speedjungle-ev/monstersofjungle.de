3cd1baf976b1ddc527e55be0de30146ff18de4c5
Philip Saa <cowglow@gmail.com>
2026-03-14 16:24:23 +0100

    Echo out location

--- src/script.ts
+++ src/script.ts
@@ -9,7 +9,7 @@ const footerEl = document.querySelector("footer");
 const hasCustomHeader = [
   `${import.meta.env.BASE_URL}/artist-details.html`,
 ].includes(window.location.pathname);
-
+console.log(window.location);
 if (headerEl && !hasCustomHeader)
   await renderTemplate("templates/header.html", headerEl);
 if (footerEl) await renderTemplate("templates/footer.html", footerEl);
